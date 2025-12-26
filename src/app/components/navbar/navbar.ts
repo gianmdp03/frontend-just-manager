600c690d38ff54f63b2b9cf86a5b454f4b388d71
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-12-25 21:08:17 -0300

    Add basic navbar

---
+++ src/app/components/navbar/navbar.ts
@@ -0,0 +1,13 @@
+import { Component, input, InputSignal } from '@angular/core';
+import { NavItem } from '../../models/nav-item';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+})
+export class Navbar {
+  //items = input.required<NavItem[]>();
+}
