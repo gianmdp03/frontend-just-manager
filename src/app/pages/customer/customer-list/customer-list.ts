7b7b0fa13b6d6685e13cb68bd4b68cd76744daee
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-12-26 12:11:15 -0300

    add half of getCustomers method

--- src/app/pages/customer/customer-list/customer-list.ts
+++ src/app/pages/customer/customer-list/customer-list.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import { CustomerService } from '../../../services/customer-service';
+import { CommonModule } from '@angular/common';
+import { CustomerDetail } from '../../../models/customer/customer-detail';
 @Component({
   selector: 'app-customer-list',
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, CommonModule],
   templateUrl: './customer-list.html',
   styleUrl: './customer-list.css',
 })
-export class CustomerList {
+export class CustomerList implements OnInit{
+  customerService = inject(CustomerService);
+  customers:CustomerDetail[] = [];
 
+  ngOnInit(): void{
+    this.getProducts();
+  }
+
+  getProducts(){
+    this.customerService.getCustomer().subscribe({
+      next: (data) => {
+        this.customers = data;
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    })
+  }
 }
--- src/app/services/customer-service.ts
+++ src/app/services/customer-service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { CustomerDetail } from '../models/customer/customer-detail';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CustomerService {
-  
+  readonly apiUrl = "http://localhost:8080/api/customers";
+  http = inject(HttpClient);
+
+  getCustomer(){
+    return this.http.get<CustomerDetail[]>(this.apiUrl);
+  }
 }
