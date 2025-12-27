1f59c158e25e9d69c814b9821d608cb5a637eb3d
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-12-27 14:16:40 -0300

    customer detail not finished

--- src/app/pages/customer/customer-detail/customer-detail.ts
+++ src/app/pages/customer/customer-detail/customer-detail.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { OrderService } from '../../../services/order-service';
+import { CustomerService } from '../../../services/customer-service';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-customer-detail',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './customer-detail.html',
   styleUrl: './customer-detail.css',
 })
-export class CustomerDetail {
+export class CustomerDetail implements OnInit{
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
+  customer = signal<CustomerDetail|null>(null);
+  orderService = inject(OrderService);
+  customerService = inject(CustomerService);
 
+  ngOnInit(): void {
+    this.getCustomerById();
+  }
+
+  getCustomerById(){
+    const id = this.route.snapshot.paramMap.get("id");
+    if(id){
+      this.customerService.getCustomer(id).subscribe({
+        next:(data)=>{
+          this.customer.set(data);
+        },
+        error:(error)=>{
+          console.log(error);
+        }
+      });
+    }
+  }
 }
